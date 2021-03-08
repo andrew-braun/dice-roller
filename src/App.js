f8875161f19a67032914ceafbb34c305ad382154
Andrew Braun <andrew@andrewbraun.dev>
2021-03-08 10:32:08 +0400

    feat: Dynamically generate FA die icon based on prop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react"
 import Layout from "./components/Layout/Layout"
 import Die from "./components/Die/Die"
+import Button from "./components/Button/Button"
 import "./App.css"
 
 class App extends Component {
+	state = {
+		dieOne: 1,
+	}
 	render() {
 		return (
 			<Layout>
 				<div className="App">
-					<Die />
+					<Die number={this.state.dieOne} />
+					<Button />
 				</div>
 			</Layout>
 		)
--- src/components/Die/Die.js
+++ src/components/Die/Die.js
@@ -1,21 +1,24 @@
 import React, { Component } from "react"
-import {
-	FaDiceOne,
-	FaDiceTwo,
-	FaDiceThree,
-	FaDiceFour,
-	FaDiceFive,
-	FaDiceSix,
-} from "react-icons/fa"
+import * as FontAwesome from "react-icons/fa"
 import styles from "./die.module.css"
 
 class Die extends Component {
 	render() {
-		return (
-			<div className={styles.die}>
-				<FaDiceOne />
-			</div>
+		const numLookup = {
+			1: "One",
+			2: "Two",
+			3: "Three",
+			4: "Four",
+			5: "Five",
+			6: "Six",
+		}
+
+		// const die = `<FaDice${numLookup[this.props.number]} />`
+		const die = React.createElement(
+			FontAwesome[`FaDice${numLookup[this.props.number]}`]
 		)
+
+		return <div className={styles.die}>{die}</div>
 	}
 }
 
