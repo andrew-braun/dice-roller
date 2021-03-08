cf2e98e275cc1800ceb4c7fdbf426f43232bba05
Andrew Braun <andrew@andrewbraun.dev>
2021-03-08 10:37:34 +0400

    feat: Implement dice roll functionality

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ import Button from "./components/Button/Button"
 import "./App.css"
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+		this.handleRoll = this.handleRoll.bind(this)
+	}
+
 	state = {
 		dieOne: 1,
 	}
+
+	handleRoll() {
+		const roll = Math.floor(Math.random() * 6) + 1
+		this.setState({ dieOne: roll })
+		console.log(roll)
+	}
+
 	render() {
 		return (
 			<Layout>
 				<div className="App">
 					<Die number={this.state.dieOne} />
-					<Button />
+					<Button onClick={this.handleRoll} />
 				</div>
 			</Layout>
 		)
